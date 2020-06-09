80c07a8001c2759ba19f1615c11ea878e76644f0
daoerche <654871442@qq.com>
2020-06-10 00:50:44 +0800

    feature(math): 增加leetCode 167算法

---
+++ Algorithm/Math/math.js
@@ -0,0 +1,22 @@
+/**
+ * leetCode
+ * 167. 两数之和 II - 输入有序数组
+ */
+/**
+ * 非递归算法 杨氏矩阵
+ * @param {number[]} numbers
+ * @param {number} target
+ * @return {number[]}
+ */
+const twoSum = function(numbers, target) {
+    let i = 0;
+    let j = numbers.length - 1;
+    let sum = numbers[i] + numbers[j];
+
+    while (sum !== target) {
+        sum < target ? i++ : j--;
+        sum = numbers[i] + numbers[j];
+    }
+
+    return [i + 1, j + 1];
+};
