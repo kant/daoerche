4ef71daf13584e9f9d337d70666669a34c1804fc
daoercheyu <daoercheyu@tencent.com>
2020-06-14 05:04:08 +0800

    feature(algorithm): 增加几道算法

--- Algorithm/Math/math.js
+++ Algorithm/Math/math.js
@@ -20,3 +20,52 @@ const twoSum = function(numbers, target) {
 
     return [i + 1, j + 1];
 };
+
+
+/**
+ * leetCode
+ * 172. 阶乘后的零
+ */
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var trailingZeroes = function(n) {
+    let count = 0;
+    while (n >= 5) {
+        count += Math.floor(n / 5);
+        n = Math.floor(n / 5);
+    }
+
+    return count;
+};
+
+
+/**
+ * leetCode
+ * 169. 多数元素
+ */
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var majorityElement = function(nums) {
+    let num = nums[0];
+    let count = 1;
+
+    for (let i = 1, len = nums.length; i < len; i++) {
+        const curNum = nums[i];
+        if (num === curNum) {
+            count++;
+        } else {
+            if (count >= 1) {
+                count--;
+            } else {
+                num = curNum;
+                count = 1;
+            }
+        }
+    }
+
+    return num;
+};
