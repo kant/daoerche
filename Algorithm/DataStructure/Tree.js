afa201715d3a343e076e19936366920ba2602a7a
daoerche <daoerche@qq.com>
2020-07-31 10:56:23 +0800

    feature(tree): 增加面试题

--- Algorithm/DataStructure/Tree.js
+++ Algorithm/DataStructure/Tree.js
@@ -213,11 +213,43 @@ const tree = new Tree(treeArray);
  */
 // tree.DLR(tree.root, console.log);
 // tree.LDR(tree.root, console.log);
-tree.LRD(tree.root, console.log);
+// tree.LRD(tree.root, console.log);
 
 /**
  * DLR iteration
  */
 // tree.iterationDLR(tree.root, console.log);
 // tree.iterationLDR(tree.root, console.log);
-tree.iterationLRD(tree.root, console.log);
+// tree.iterationLRD(tree.root, console.log);
+
+
+/**
+ * @title: 蛇形遍历Tree结构
+ */
+const snakeTraverse = root => {
+    let queue = [root];
+    const res = [];
+
+    let flag = true;
+    while (queue.length) {
+        let temp = [];
+
+        while (queue.length) {
+            let cur = null;
+            cur = flag ? queue.pop() : queue.shift();
+            if (!cur) continue;
+            // console.log(cur.val);
+            res.push(cur.val);
+
+            temp.push(cur.left);
+            temp.push(cur.right);
+        }
+
+        queue = temp;
+        flag = !flag;
+    }
+
+    return res;
+}
+
+console.log(snakeTraverse(tree.root));
---
+++ test.js
@@ -0,0 +1,52 @@
+const fetch = (url, timeout) => {
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', url);
+
+    let now = new Date().getTime();
+    let res = xhr.send()
+
+    return new Promise((res, rej) => {
+        const checkTimeout = () => {
+            if (new Date().getTime() - now < timeout) {
+                requestAnimationFrame(checkTimeout);
+            }
+
+            rej(new Error('timeout!'));
+        }
+
+        requestAnimationFrame(checkTimeout);
+        res(res);
+    })
+}
+
+
+const sleep = time => {
+    const now = new Date().getTime();
+
+    while (new Date().getTime() - now < time) { }
+}
+
+const a = showArr => {
+    let res = [];
+
+    for (let i = showArr.length - 1; i >= 0; i--) {
+        let cur = showArr[i];
+
+
+        res.unshift(cur);
+
+        if (i > 0) {
+            let last = res.pop();
+            res.unshift(last);
+        }
+    }
+
+    return res;
+}
+
+// console.log(a([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]));
+
+
+
+
+console.log(snakeTraverse())
