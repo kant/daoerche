a9c03156800a2b3c94899849148ea720a05b08c9
daoerche <daoerche@qq.com>
2020-11-12 00:13:12 +0800

    feat(algorithm): 使用reduce重写 phoneNumber题目

--- Algorithm/Recursion/letterCombinationsOfAPhoneNumber.js
+++ Algorithm/Recursion/letterCombinationsOfAPhoneNumber.js
@@ -3,6 +3,7 @@
  * @param {string} digits
  * @return {string[]}
  */
+// 回溯
 const letterCombinations = function(digits) {
     if (digits.length === 0) return [];
 
@@ -32,6 +33,31 @@ const letterCombinations = function(digits) {
     generate(group, 0, '');
 
     return result;
+};
+
+// 使用reduce累加
+const letterCombinations2 = function(digits) {
+    if (digits.length === 0) return [];
+
+    const charMap = {
+        2: ['a', 'b', 'c'],
+        3: ['d', 'e', 'f'],
+        4: ['g', 'h', 'i'],
+        5: ['j', 'k', 'l'],
+        6: ['m', 'n', 'o'],
+        7: ['p', 'q', 'r', 's'],
+        8: ['t', 'u', 'v'],
+        9: ['w', 'x', 'y', 'z']
+    };
+
+    return digits
+        .split('')
+        .map(num => charMap[num])
+        .reduce((acc, cur) => {
+            const res = [];
+            acc.forEach(c1 => res.push(...cur.map(c2 => c1 + c2)));
+            return res;
+        });
 };
 
 console.log(letterCombinations('238'));
