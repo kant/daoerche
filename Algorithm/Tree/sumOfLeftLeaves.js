5bd969ca307652854db919015e790316ed3e5acd
daoerche <daoerche@qq.com>
2020-12-25 22:26:50 +0800

    feat(algorithm): 继续虐简单二叉树题目

---
+++ Algorithm/Tree/sumOfLeftLeaves.js
@@ -0,0 +1,36 @@
+const {
+    TreeNode,
+    createBinaryTree,
+    printTree
+} = require('../Utils/tree');
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @title Leetcode: 404. 左叶子之和
+ * @param {TreeNode} root
+ * @return {number}
+ */
+const sumOfLeftLeaves = function(root) {
+    const traverse = (node, isLeft) => {
+        if (!node) return 0;
+
+        // 叶子节点
+        if (!node.left && !node.right) {
+            return isLeft ? node.val : 0;
+        }
+
+        return traverse(node.left, true) + traverse(node.right, false);
+    }
+
+    return traverse(root, false);
+};
+
+const input = [3, 9, 20, null, null, 15, 7];
+const root = createBinaryTree(...input);
+console.log(sumOfLeftLeaves(root));
